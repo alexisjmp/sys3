0da986624a41e7ed2106344a6149384e09a3cbe0
DiegoHuanaco <Diego.huan.ram@gmail.com>
2018-02-13 15:35:40 -0300

    detalle bootstrap

--- ui/detalle.php
+++ ui/detalle.php
@@ -1,124 +1,85 @@
 <?php ?>
 
-                                   
-                            <div class="row form-group">
-                                <div class="col-lg-12 col-md-12 col-xs-12">
-                                    <label class="col-lg-3 col-md-3 col-xs-3 control-label " >Codigo</label>
-                                    <label class="col-lg-3 col-md-3 col-xs-3 control-label " >Aduanero</label>
-                                    <label class="col-lg-3 col-md-3 col-xs-3 control-label " >de Item</label>
-                                    <label class="col-lg-3 col-md-3 col-xs-3 control-label " >de Inventario</label>
-                                    <div class="col-lg-3 col-md-3 col-xs-3">
-                                        <input type="text" id="codigo_detalle" class=" form-control" required>
-                                    </div>
-                                    <div class="col-lg-3 col-md-3 col-xs-3">
-                                        <input type="text" id="aduanero_detalle" class=" form-control" required>
-                                    </div>
-                                    <div class="col-lg-3 col-md-3 col-xs-3">
-                                        <input type="text" id="item_detalle" class=" form-control" required>
-                                    </div>
-                                    <div class="col-lg-3 col-md-3 col-xs-3">
-                                        <input type="text" id="inventario_detalle" class=" form-control" required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row form-group">
-<!--                               <div class="col-lg-4 col-md-4 col-xs-12">-->
-                                    <label class="col-lg-2 col-md-2 col-xs-3 control-label " >Codigo de producto   </label>
-                                    <div class="col-lg-2 col-md-2 col-xs-3">
-                                        <input type="text" id="codigo_de_producto" class=" form-control" required>
-                                    </div>    
-                                
-<!--                                <div class="col-lg-4 col-md-4 col-xs-12">-->
-                                    <div class="col-lg-1 col-xs-2">
-                                        <button type="button" title="Aceptar"  class="btn btn-success "
-                                                    onclick=""> <span  class="glyphicon glyphicon-search small"></span></button>
-                                    </div>
-                                    <div class="col-lg-3 col-md-3 col-xs-4">
-                                        <input type="text" id="codigo_de_producto" class=" form-control" required disabled>
-                                    </div>    
+
+<div class="row form-group">
+    <div class="col-lg-12 col-md-12 col-xs-12">
+        <label class="col-lg-3 col-md-3 col-xs-3 control-label " >Codigo</label>
+        <label class="col-lg-3 col-md-3 col-xs-3 control-label " >Aduanero</label>
+        <label class="col-lg-3 col-md-3 col-xs-3 control-label " >de Item</label>
+        <label class="col-lg-3 col-md-3 col-xs-3 control-label " >de Inventario</label>
+        <div class="col-lg-3 col-md-3 col-xs-3">
+            <input type="text" id="codigo_detalle" class=" form-control" required>
+        </div>
+        <div class="col-lg-3 col-md-3 col-xs-3">
+            <input type="text" id="aduanero_detalle" class=" form-control" required>
+        </div>
+        <div class="col-lg-3 col-md-3 col-xs-3">
+            <input type="text" id="item_detalle" class=" form-control" required>
+        </div>
+        <div class="col-lg-3 col-md-3 col-xs-3">
+            <input type="text" id="inventario_detalle" class=" form-control" required>
+        </div>
+    </div>
+</div>
+
 <!--                                </div>-->
-                                    <div class="col-lg-4 col-md-4 col-xs-10">   
-                                        <label class="col-lg-7 col-md-7 col-xs-7 control-label " >Unidad de medida</label>
-                                        <div class="col-lg-2 col-md-2 col-xs-2">
-                                            <input type="text" id="unidad" class=" form-control" required disabled>
-                                        </div>
-                                        <div class="col-lg-3 col-md-3 col-xs-3">
-                                            <input type="text" id="medida" class=" form-control" required disabled>
-                                        </div>
-                                    </div>
-                            </div>
-                            <div class="row form-group">
-<!--                               <div class="col-lg-4 col-md-4 col-xs-12">-->
-                                    <label class="col-lg-2 col-md-2 col-xs-4 control-label " >Codigo arancelario   </label>
-                                    <div class="col-lg-2 col-md-2 col-xs-3">
-                                        <input type="text" id="codigo_arancelario" class=" form-control" required disabled>
-                                    </div>
-                                    <div class="col-lg-1 col-xs-2">
-                                    </div>
+<div class="row form-group col-lg-12 col-sm-12 col-md-12 col-xs-12">   
+    <label class="col-lg-3 col-md-3 col-sm-3 col-xs-6 control-label " >Unidad de medida</label>
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+        <input type="text" id="unidad" class=" form-control" required disabled>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+        <input type="text" id="medida" class=" form-control" required disabled>
+    </div>
+</div>
+
+
 <!--                                </div>-->
-<!--                                <div class="col-lg-4 col-md-4 col-xs-12">-->
-                                    
-                                    <div class="col-lg-3 col-md-3 col-xs-4">
-                                        <input type="text" id="txt_codigo_arancelario" class=" form-control" required disabled>
-                                    </div>    
-<!--                                </div>-->
-                                    <div class="col-lg-4 col-md-4 col-xs-10">   
-                                        <label class="col-lg-7 col-md-7 col-xs-7 control-label " >Unidadades x U. de medida</label>
-                                        <div class="col-lg-5 col-md-5 col-xs-5">
-                                            <input type="text" id="unidad" class=" form-control" required >
-                                        </div>
-                                        
-                                    </div>
-                            </div>
-                            <div class="row form-group">
-<!--                               <div class="col-lg-4 col-md-4 col-xs-12">-->
-                                    <label class="col-lg-2 col-md-2 col-xs-3 control-label " >Grupo   </label>
-                                    <div class="col-lg-2 col-md-2 col-xs-3">
-                                        <input type="text" id="Grupo" class=" form-control" required>
-                                    </div>    
-<!--                                </div>-->
-<!--                                <div class="col-lg-4 col-md-4 col-xs-12">-->
-                                    <div class="col-lg-1 col-xs-2">
-                                        <button type="button" title="Aceptar"  class="btn btn-success "
-                                                    onclick=""> <span  class="glyphicon glyphicon-search small"></span></button>
-                                    </div>
-                                    <div class="col-lg-3 col-md-3 col-xs-4">
-                                        <input type="text" id="txt_aceptar" class=" form-control" required disabled>
-                                    </div>    
-<!--                                </div>-->
-                            </div>
-                            <div class="row form-group">
-                                <div class="col-lg-12 col-md-12 col-xs-12">
-                                    <label class="col-lg-3 col-md-3 col-xs-3 control-label " >Nombre Producto</label>
-                                    <label class="col-lg-3 col-md-3 col-xs-3 control-label " >Modelo</label>
-                                    <label class="col-lg-3 col-md-3 col-xs-3 control-label " >Tipo</label>
-                                    <label class="col-lg-3 col-md-3 col-xs-3 control-label " >Marca</label>
-                                    <div class="col-lg-3 col-md-3 col-xs-3">
-                                        <input type="text" id="codigo_detalle" class=" form-control" required>
-                                    </div>
-                                    <div class="col-lg-3 col-md-3 col-xs-3">
-                                        <input type="text" id="aduanero_detalle" class=" form-control" required>
-                                    </div>
-                                    <div class="col-lg-3 col-md-3 col-xs-3">
-                                        <input type="text" id="item_detalle" class=" form-control" required>
-                                    </div>
-                                    <div class="col-lg-3 col-md-3 col-xs-3">
-                                        <input type="text" id="inventario_detalle" class=" form-control" required>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row form-group">
-                                <label class="col-lg-3 col-md-3 col-xs-6 control-label " >Informe adicional   </label>
-                                    <div class="col-lg-4 col-md-4 col-xs-6">
-                                        <input type="text" id="Grupo" class=" form-control" required>
-                                    </div>
-                            </div>
-                            <div class="row form-group">
-                                <label class="col-lg-3 col-md-3 col-xs-6 control-label " >Numero de serie  </label>
-                                    <div class="col-lg-4 col-md-4 col-xs-6">
-                                        <input type="text" id="Grupo" class=" form-control" required>
-                                    </div>
-                            </div>
+<div class="row form-group col-lg-12 col-sm-12 col-md-12 col-xs-12">   
+    <label class="col-lg-3 col-md-3 col-xs-6 col-sm-3 control-label " >Unidadades x U. de medida</label>
+    <div class="col-lg-3 col-md-3  col-sm-3 col-xs-5">
+        <input type="text" id="unidad" class=" form-control" required >
+    </div>
+
+</div>
+
+
+<div class="row form-group">
+    <div class="col-lg-12 col-md-12 col-xs-12">
+        <label class="col-lg-3 col-md-3 col-xs-3 control-label " >N. P.</label>
+        <label class="col-lg-3 col-md-3 col-xs-3 control-label " >Modelo</label>
+        <label class="col-lg-3 col-md-3 col-xs-3 control-label " >Tipo</label>
+        <label class="col-lg-3 col-md-3 col-xs-3 control-label " >Marca</label>
+        <div class="col-lg-3 col-md-3 col-xs-3">
+            <input type="text" id="codigo_detalle" class=" form-control" required>
+        </div>
+        <div class="col-lg-3 col-md-3 col-xs-3">
+            <input type="text" id="aduanero_detalle" class=" form-control" required>
+        </div>
+        <div class="col-lg-3 col-md-3 col-xs-3">
+            <input type="text" id="item_detalle" class=" form-control" required>
+        </div>
+        <div class="col-lg-3 col-md-3 col-xs-3">
+            <input type="text" id="inventario_detalle" class=" form-control" required>
+        </div>
+    </div>
+</div>
+<div class="row form-group">
+    <div class="col-lg-12 col-md-12 col-xs-12">
+        <label class="col-lg-3 col-md-3 col-sm-3 col-xs-6 control-label " >Informe adicional   </label>
+        <div class="col-lg-4 col-sm-4 col-md-4 col-xs-6">
+            <input type="text" id="Grupo" class=" form-control" required>
+        </div>
+    </div>
+</div>
+<div class="row form-group">
+    <div class="col-lg-12 col-md-12 col-xs-12">
+
+        <label class="col-lg-3 col-sm-3 col-md-3 col-xs-6 control-label " >Numero de serie  </label>
+        <div class="col-lg-4 col-md-4 col-xs-6 col-sm-4">
+            <input type="text" id="Grupo" class=" form-control" required>
+        </div></div>
+</div>
 
 
 
